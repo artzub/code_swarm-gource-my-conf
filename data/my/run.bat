c499b0d162aa4b2abb6dd87caf9cacc3a6a62b4b
artzub <artzub@gmail.com>
2011-02-22 01:43:28 +0700

    добавлены: 1. компонетны для генерации видео 2. gource 3. изменен run.bat

--- data/my/run.bat
+++ data/my/run.bat
@@ -7,19 +7,22 @@ ren data\actions_res.xml actions.xml
 
 call genlogforgourcefromcodeswarm.exe data\actions.xml
 
+pushd png
+del *.png
+popd
 
 pushd ..\..
 call run.bat data\my\my.config
 popd
 
 pushd png
-call "d:\showteamwork\tools\nt\mencoder" mf://*.png -mf fps=19:type=png -ovc x264 -x264encopts pass=1:bitrate=1000 -oac copy -audiofile "..\data\audio.wav" -o "..\results\result.avi"
+call "..\tools\nt\mencoder" mf://*.png -mf fps=19:type=png -ovc x264 -x264encopts pass=1:bitrate=1000 -oac copy -audiofile "..\data\audio.wav" -o "..\results\result.avi"
 popd
 
-call gource --user-image-dir "logos" --follow-user "artzub" --hide filenames,dirnames --default-user-image "default.png" --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "results\resultgource.ppm" "data\gource.log"
+call gource --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "results\resultgource.ppm" "data\gource.log"
+rem --user-image-dir "logos" --follow-user "artzub" --default-user-image "default.png" 
+call "..\tools\nt\ffmpeg" -y -b 9000K -f image2pipe -vcodec ppm -i "results\resultgource.ppm" -fpre "..\tools\ll.ffpreset" -i "results\resultgource.ppm" -vcodec libx264 "results\resultgource.avi"
 
-call "d:\showteamwork\tools\nt\ffmpeg" -y -b 9000K -f image2pipe -vcodec ppm -i "results\resultgource.ppm" -fpre "D:\showteamwork\tools\ll.ffpreset" -i "results\resultgource.ppm" -vcodec libx264 "results\resultgource.avi"
+call "..\tools\nt\mencoder" "results\resultgource.avi" -ovc x264 -x264encopts pass=1:bitrate=10000 -ofps 19 -speed 2 -o "results\resultgource.fps"
 
-call "d:\showteamwork\tools\nt\mencoder" "results\resultgource.avi" -ovc x264 -x264encopts pass=1:bitrate=10000 -ofps 19 -speed 2 -o "results\resultgource.fps"
-
-call "d:\showteamwork\tools\nt\mencoder" "results\resultgource.fps" -ovc x264 -x264encopts pass=1:bitrate=10000 -oac copy -audiofile "data\audio.wav" -o "results\resultgource.avi"
+call "..\tools\nt\mencoder" "results\resultgource.fps" -ovc x264 -x264encopts pass=1:bitrate=10000 -oac copy -audiofile "data\audio.wav" -o "results\resultgource.avi"
