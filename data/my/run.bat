d4351155719789c9fd579213f6dc5485e745f0e3
artzub <artzub@gmail.com>
2011-02-23 02:48:58 +0700

    в gen_log изменен алгоритм генерации взят предложенный http://bliznezz.habrahabr.ru/ в sort_log добавлена сортировака по статусу в generatorlog поправлен косяк генерации статуса.

--- data/my/run.bat
+++ data/my/run.bat
@@ -1,15 +1,11 @@
-rem del data\actions_orign.xml
-rem ren data\actions.xml actions_orign.xml
-
-rem call sortDate.exe data\actions_orign.xml
-
-rem ren data\actions_res.xml actions.xml
+:: del data\actions_orign.xml
+:: ren data\actions.xml actions_orign.xml
+:: call sortDate.exe data\actions_orign.xml
+:: ren data\actions_res.xml actions.xml
 
 call sh gen_log ./data/actions.xml
 
-call sh sort_log ./data/actions.log
-
-del data\actions.log
+call sh sort_log ./data/actions.log > data\gource.log
 
 pushd png
 del *.png
@@ -24,7 +20,7 @@ call "..\tools\nt\mencoder" mf://*.png -mf fps=19:type=png -ovc x264 -x264encopt
 popd
 
 call "..\tools\gource\gource.exe" --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "results\resultgource.ppm" "data\gource.log"
-rem --user-image-dir "logos" --follow-user "artzub" --default-user-image "default.png" 
+::--user-image-dir "logos" --follow-user "artzub" --default-user-image "default.png" 
 call "..\tools\nt\ffmpeg" -y -b 9000K -f image2pipe -vcodec ppm -i "results\resultgource.ppm" -fpre "..\tools\ll.ffpreset" -i "results\resultgource.ppm" -vcodec libx264 "results\resultgource.avi"
 
 call "..\tools\nt\mencoder" "results\resultgource.avi" -ovc x264 -x264encopts pass=1:bitrate=10000 -ofps 19 -speed 2 -o "results\resultgource.fps"
@@ -33,3 +29,4 @@ call "..\tools\nt\mencoder" "results\resultgource.fps" -ovc x264 -x264encopts pa
 
 del results\resultgource.ppm
 del results\resultgource.fps
+del data\actions.log
