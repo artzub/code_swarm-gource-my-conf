7b00a1d73add59ca533ad4ff814d1abc648b4e44
artzub <artzub@gmail.com>
2011-02-22 15:06:28 +0700

    добавлен скрипт для генерации log для gource добавлен скрипт для сортировки log gource run.bat изменен так, что используются скрипты для sh добавлен скрипт для генерации лога строк.

--- data/my/run.bat
+++ data/my/run.bat
@@ -1,11 +1,15 @@
-del data\actions_orign.xml
-ren data\actions.xml actions_orign.xml
+rem del data\actions_orign.xml
+rem ren data\actions.xml actions_orign.xml
 
-call sortDate.exe data\actions_orign.xml
+rem call sortDate.exe data\actions_orign.xml
 
-ren data\actions_res.xml actions.xml
+rem ren data\actions_res.xml actions.xml
 
-call genlogforgourcefromcodeswarm.exe data\actions.xml
+call sh gen_log ./data/actions.xml
+
+call sh sort_log ./data/actions.log
+
+del data\actions.log
 
 pushd png
 del *.png
@@ -19,10 +23,13 @@ pushd png
 call "..\tools\nt\mencoder" mf://*.png -mf fps=19:type=png -ovc x264 -x264encopts pass=1:bitrate=1000 -oac copy -audiofile "..\data\audio.wav" -o "..\results\result.avi"
 popd
 
-call gource --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "results\resultgource.ppm" "data\gource.log"
+call "..\tools\gource\gource.exe" --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "results\resultgource.ppm" "data\gource.log"
 rem --user-image-dir "logos" --follow-user "artzub" --default-user-image "default.png" 
 call "..\tools\nt\ffmpeg" -y -b 9000K -f image2pipe -vcodec ppm -i "results\resultgource.ppm" -fpre "..\tools\ll.ffpreset" -i "results\resultgource.ppm" -vcodec libx264 "results\resultgource.avi"
 
 call "..\tools\nt\mencoder" "results\resultgource.avi" -ovc x264 -x264encopts pass=1:bitrate=10000 -ofps 19 -speed 2 -o "results\resultgource.fps"
 
 call "..\tools\nt\mencoder" "results\resultgource.fps" -ovc x264 -x264encopts pass=1:bitrate=10000 -oac copy -audiofile "data\audio.wav" -o "results\resultgource.avi"
+
+del results\resultgource.ppm
+del results\resultgource.fps
