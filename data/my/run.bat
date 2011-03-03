29dd2691cbf5f9567dcc6a2d158278fdda42e15d
artzub <artzub@gmail.com>
2011-03-03 17:03:52 +0700

    у gource уменьшена светимость элементов. удаление файла ppm сразу как он обработан.

--- data/my/run.bat
+++ data/my/run.bat
@@ -24,18 +24,19 @@ del *.png
 popd
 
 pushd "tools\gource"
-call gource.exe --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "..\..\results\resultgource.ppm" "..\..\data\gource.log"
+call gource.exe --bloom-intensity 0.35 -b 333333 --hide filenames,dirnames --user-scale 2 --output-framerate 25 --stop-position 1 --highlight-all-users --seconds-per-day 1 --output-ppm-stream "..\..\results\resultgource.ppm" "..\..\data\gource.log"
 ::--user-image-dir "logos" --follow-user "artzub" --default-user-image "default.png" 
 popd
 
 pushd "tools\nt"
 call ffmpeg -y -b 9000K -f image2pipe -vcodec ppm -i "..\..\results\resultgource.ppm" -fpre "..\ll.ffpreset" -i "..\..\results\resultgource.ppm" -vcodec libx264 "..\..\results\resultgource.avi"
 
+del results\resultgource.ppm
+
 call mencoder "..\..\results\resultgource.avi" -ovc x264 -x264encopts pass=1:bitrate=10000 -ofps 19 -speed 2 -o "..\..\results\resultgource.fps"
 
 call mencoder "..\..\results\resultgource.fps" -ovc x264 -x264encopts pass=1:bitrate=10000 -oac copy -audiofile "..\..\data\audio.wav" -o "..\..\results\resultgource.avi"
 popd
 
-del results\resultgource.ppm
 del results\resultgource.fps
 del data\actions.log
